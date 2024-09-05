538b6e2ca3022a538f6e96c5843e8c86d65aefc2
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-09-05 15:04:37 +0500

    remove conflict

--- conf/urls.py
+++ conf/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
@@ -25,3 +26,9 @@ urlpatterns = [
     path('sale_marketing/', include('promotion.urls')),
 
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
