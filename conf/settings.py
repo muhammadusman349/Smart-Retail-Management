b5570bc2df1d668fb7b53c324a1ad8f4f376ff2e
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-09-05 15:00:46 +0500

    conflict

--- conf/settings.py
+++ conf/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -37,6 +39,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'queryhunter',
+    'django_celery_beat',
+    'django_celery_results',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'account',
+    'AI',
+    'inventory',
+    'promotion'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "account.custom_queryhunter_middleware.CustomQueryHunterMiddleware"
 ]
 
 ROOT_URLCONF = "conf.urls"
@@ -76,7 +88,7 @@ WSGI_APPLICATION = "conf.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -121,3 +133,63 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'm.maher0044@gmail.com'
+EMAIL_HOST_PASSWORD = "ooep jzeq roud cptl"
+DEFAULT_FROM_EMAIL = 'm.maher0044@gmail.com'
+
+# Celery settings
+CELERY_BROKER_URL = "amqp://localhost:5672"
+CELERY_RESULT_BACKEND = "django-db"
+
+# broker_connections_retry_on_startup
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+
+# Optional Celery settings
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
+# Celery Beat
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+SITE_NAME = "SRMS"
+SITE_URL = "http://localhost:8000/inventory"
+
+
+CELERY_BEAT_SCHEDULE = {
+    'schedule-promotions-daily': {
+        'task': 'promotion.tasks.schedule_all_promotions',
+        'schedule': crontab(hour=0, minute=0),  # Run daily
+    },
+    'update-coupon-statuses-daily': {
+        'task': 'promotion.update_coupon_statuses',
+        'schedule': crontab(hour=0, minute=0),  # Run daily
+    },
+    'check-campaign-status-daily': {
+        'task': 'promotion.tasks.check_campaign_status',
+        'schedule': crontab(hour=0, minute=0),  # Run daily
+    },
+    'deactivate-expired-campaigns-daily': {
+        'task': 'promotion.tasks.deactivate_expired_campaigns',
+        'schedule': crontab(hour=0, minute=0),  # Run daily
+    },
+}
+
+QUERYHUNTER_BASE_DIR = BASE_DIR
+
+
+if DEBUG:
+    QUERYHUNTER = {
+        'ENABLED': True,
+        'LOG_LEVEL': 'WARNING',
+        'IGNORE_NAMES': [],
+        'SLOW_QUERY_TIME': 100,
+        'DUPLICATE_QUERY_LIMIT': 2,
+    }
